a3b932d5e6e9da4c59a0b99b8d1a226563572169
P_991225 <P_991225@DC-350672.redecamara.camara.gov.br>
2019-07-29 17:10:25 -0300

    Adição do EmpresaRepository

--- src/main/java/com/estudos/api/repositories/EmpresaRepository.java
+++ src/main/java/com/estudos/api/repositories/EmpresaRepository.java
@@ -1,5 +1,11 @@
 package com.estudos.api.repositories;
 
-public interface EmpresaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estudos.api.entities.Empresa;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 	
 }

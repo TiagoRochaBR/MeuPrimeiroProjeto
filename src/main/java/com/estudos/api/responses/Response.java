e68019031d449d57daab78faf0419533ddb698ef
P_991225 <P_991225@DC-350672.redecamara.camara.gov.br>
2019-07-31 15:55:42 -0300

    Ajustes na classe

--- src/main/java/com/estudos/api/responses/Response.java
+++ src/main/java/com/estudos/api/responses/Response.java
@@ -3,7 +3,6 @@ package com.estudos.api.responses;
 import java.util.ArrayList;
 import java.util.List;
 
-<<<<<<< HEAD
 import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.Getter;
@@ -16,15 +15,6 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 @XmlRootElement
-=======
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
->>>>>>> b1c558ddbe43b50907a040b6252ba7cab9b959fa
 public class Response<T> {
 	
 	private T data;
